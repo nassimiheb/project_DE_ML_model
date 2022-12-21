3c55ab646634464e87c7dcaa02005ab28c583fcd
nassimiheb <hi_aouadj@esi.dz>
2022-12-21 12:34:36 -0800

    feat: add roc-auc curve and the confusion matrix of the model

--- training.py
+++ training.py
@@ -1,8 +1,10 @@
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, classification_report, classification_report, roc_auc_score,roc_curve,auc
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def fit(X, y, ts, rs, model,accuracy_list ,f1_list):
+def fit(X, y, ts, rs, model,accuracy_list ,f1_list,roc_auc_list):
     #train test split
     (X_train, X_test, y_train, y_test) = get_train_test_split(X, y, ts, rs)
     
@@ -31,6 +33,28 @@ def fit(X, y, ts, rs, model,accuracy_list ,f1_list):
     print("F1 Score: ", f1)
     print("-" * 100)
     print()
+
+     #roc-auc curve of model
+    fpr,tpr,threshold = roc_curve(y_test,pred)
+    auc_value = auc(fpr,tpr)
+    rocauc_score = roc_auc_score(y_test, pred)
+    roc_auc_list.append(rocauc_score)
+    plt.figure(figsize=(5,5),dpi=100)
+    print("ROC-AUC Score: ", f1)
+    print("-" * 100)
+    print()
+    plt.plot(fpr,tpr,linestyle='-',label = "(auc_value = %0.3f)" % auc_value)
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.legend()
+    plt.show()
+    print()
+    
+    #confusion matrix for model
+    print("Confusion Matrix: ")
+    plt.figure(figsize=(10, 5))
+    sns.heatmap(confusion_matrix(y_test, pred), annot=True, fmt='g');
+    plt.title('Confusion Matrix', fontsize=20)
 
 def get_train_test_split(x_input, y_target, test_size, random_state):
     return train_test_split(x_input, y_target, test_size=test_size, stratify=y_target, random_state=random_state)
